c5fcbe7220185015fa02c29aa2dc92181d8e4d1e
Luke Cheng <Lukeecheng@gmail.com>
2024-07-31 21:28:10 -0700

    added CORS handling

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, jsonify
 from chat import get_response
 import requests
 from bs4 import BeautifulSoup
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.get("/")
 def index_get():
